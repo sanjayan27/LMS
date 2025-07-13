3bdd3f2985335c9d7f0b5af1be78a93084339ae2
sanjay <sanjayansanjay308@gmail.com>
2025-07-13 20:27:28 +0530

    recent update

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 export const connectMongoDb=async()=>{
     try{
-        const connect = await mongoose.connect(process.env.MONGO_URL/"lms")
+        const connect = await mongoose.connect(process.env.MONGO_URL)
             console.log('mongodb is connected')
 
     }catch(err){
