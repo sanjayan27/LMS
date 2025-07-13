5f6d37492202f3cae8940981a20d829201420da8
sanjay <sanjayansanjay308@gmail.com>
2025-07-13 18:26:06 +0530

    route  is updated

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectMongoDb } from './config/mongoDB.js'
+import { clerkWebhooks } from './controller/webhook.js'
 dotenv.config()
 
 const app = express()
@@ -16,6 +17,7 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send(" Hello Express")
 })
+app.post('/clerk',clerkWebhooks)
 
 
 
